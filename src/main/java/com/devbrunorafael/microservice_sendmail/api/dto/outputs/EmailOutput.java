1ebbca68d4d3bb6bffc4aa1256b19d865548d7a9
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-17 19:21:56 -0300

    email sending implemented

--- src/main/java/com/devbrunorafael/microservice_sendmail/api/dto/outputs/EmailOutput.java
+++ src/main/java/com/devbrunorafael/microservice_sendmail/api/dto/outputs/EmailOutput.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 @Setter
 public class EmailOutput {
 
-    private String emailFrom;
     private String emailTo;
     private String subject;
     private String text;
